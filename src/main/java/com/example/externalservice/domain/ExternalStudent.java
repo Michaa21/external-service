ad4f8bebe295e29b850085391b9b736f0bd52c8c
Mikhail Lukin <21lukin21@gmail.com>
2026-04-23 11:20:30 +0500

    fix

--- src/main/java/com/example/externalservice/domain/ExternalStudent.java
+++ src/main/java/com/example/externalservice/domain/ExternalStudent.java
@@ -1,5 +1,6 @@
 package com.example.externalservice.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -17,7 +18,9 @@ public class ExternalStudent {
     @GeneratedValue
     private UUID id;
 
+    @Column(nullable = false, unique = true)
     private UUID studentId;
 
+    @Column(nullable = false)
     private String extraInfo;
 }
